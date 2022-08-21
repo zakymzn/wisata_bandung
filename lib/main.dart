f749ff201ae94466e05d36a7b42fefe6cd5c8ebd
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-08-21 14:42:12 +0700

    menambahkan navigasi, tombol back, tombol favorit, dan menambahkan daftar tempat wisata lainnya

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_bandung/detail_screen.dart';
+// import 'package:wisata_bandung/detail_screen.dart';
+import 'package:wisata_bandung/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
